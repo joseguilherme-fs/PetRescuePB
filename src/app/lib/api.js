208728a3fd6f5b20bf56a51edb6e1f51e2b2f490
José Guilherme <jguilherme.s2@gmail.com>
2023-06-24 21:01:00 -0300

    Tentando colocar em React

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,17 +1,33 @@
 import { createClient } from '@supabase/supabase-js';
 
-const API_KEY = SUPABASE_ANON_KEY;
-const API_URL = SUPABASE_URL;
-
+const API_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const API_URL = process.env.REACT_APP_SUPABASE_URL;
 const supabase = createClient(API_URL, API_KEY);
 
-async function insertData(dado) {
-    let { data: AnimaisDesaparecidos, error } = await _supabase.from('AnimaisDesaparecidos').insert([dado])
-    if (error){
-      console.log(error)
-      return
-    } else {
-      window.alert('Informações salvas!')
-      loadData();
-    }
+async function createAnimalDesaparecido(data) {
+  const { data: newAnimalDesaparecido, error } = await supabase
+    .from('AnimaisDesaparecidos')
+    .insert(data);
+
+  if (error) {
+    throw error;
   }
+
+  return newAnimalDesaparecido;
+}
+
+async function getAnimaisDesaparecidos() {
+  const { data, error } = await supabase.from('AnimaisDesaparecidos').select();
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
+
+export default {
+  createAnimalDesaparecido,
+  getAnimaisDesaparecidos,
+};
+
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Links from './components/Links'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Image from 'next/image';
+import styles from './page.module.css';
+import Links from './components/Links';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import supabase from './/lib/api';
 
-export default function Home() {
+
+const Page = () => {
+  const [animaisDesaparecidos, setAnimaisDesaparecidos] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const { data, error } = await _supabase.from('AnimaisDesaparecidos').select();
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.table(data);
+    setAnimaisDesaparecidos(data || []);
+  };
+
+  
+
   return (
     <>
   <Links />
@@ -47,6 +67,50 @@ export default function Home() {
     </section>
   </div>
 
+
+  <div className="posicionamento">
+    <section className="navegacao">
+      <nav>
+        <ul>
+          <div className="barra">
+            <form className="pesquisa">
+              <input
+                type="text"
+                className="search"
+                placeholder="busque por nomes de cidades e animais"
+              />
+              <button className="botaobusca">
+                <i className="fa-solid fa-magnifying-glass" />
+              </button>
+            </form>
+          </div>
+          <div className="botaopiloto">
+            <li>
+              <button className="botao" id="btnReg">
+                Registre-se
+              </button>
+            </li>
+            <li>
+              <a href="adote.html">
+                <button className="botao" id="btnAdote">
+                  {" "}
+                  Adote!
+                </button>
+              </a>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </section>
+  </div>
+   {/* ... */}
+   <Modal loadData={loadData} />
+      {/* ... */}
+      <section className="animais" id="animaisdesap">
+        {animaisDesaparecidos.map((animal) => (
+          <AnimalCardDesaparecido key={animal.nome} animal={animal} />
+        ))}
+      </section>
   <section id="desaparecidos">
     <div style={{ marginBottom: 30 }}>
       <h1 className="titulo1">
